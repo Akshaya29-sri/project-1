fb9d8bbbccdf53696d7cd679cee4bd1475fd1173
akshaya <akshaya.pra12@gmail.com>
2025-03-01 13:29:10 +0100

    swapping unsuccesful

--- candy.js
+++ candy.js
@@ -5,6 +5,7 @@ const columns=9;
 let score=0; 
 let currTile;
 let otherTile;
+//window.currTile=this;
 
 window.onload=function(){
     startGame();
@@ -19,29 +20,80 @@ function startGame(){
             let tile=document.createElement("img");
           tile.id=r.toString() + "-" + c.toString();
           tile.src="images/" + randomCandy() + ".png";
+          tile.setAttribute("draggable","true");
 
-          /*tile.addEventListener("dragStart",dragStart);
-          tile.addEventListener("dragOver",dragOver);
-          tile.addEventListener("dragEnter",dragEntert);
-          tile.addEventListener("dragLeave",dragLeave);
-          tile.addEventListener("drop",drop);
-          tile.addEventListener("dragEnd",dragEnd);*/
+          tile.addEventListener("dragstart", function(){
+            console.log("Drag Started on",this.id);
+          });
+          console.log("added",tile.id);
+          tile.addEventListener("dragover",dragOver);
+          tile.addEventListener("dragenter",dragEnter);
+          tile.addEventListener("dragleave",dragLeave);
+          tile.addEventListener("drop",dragDrop);
+          tile.addEventListener("dragend",dragEnd);
 
 
-          document.getElementById("board").append(tile);
+          document.getElementById("board").appendChild(tile);
           row.push(tile);
 
         }
         board.push(row);
     }
-    console.log(board);
+    console.log(board,"Board created successfully");
 }
 function dragStart(){
-    currTile=this;
+    //console.log("drag started on",this.id);
+    /*if(!this){
+        console.log("error");
+        return;
+    }*/
+    currTile = this;
+    //console.log("drag start-currTile set to",currTile.id)
+    window.currTile=this;
+    console.log("drag start-currTile set to",currTile.id)
 }
 function dragOver(e){
     e.preventDefault();
 }
+function dragEnter(e){
+e.preventDefault();
+}
 function dragLeave(){
+
+}
+function dragDrop(){
+    //otherTile=this;
+    console.log("dropped on",this.id);
+    otherTile=this;
+}
+function dragEnd(){
+    console.log("drag End trigerred");
+    currTile=window.currTile || currTile;
+    //console.log("currTile:",currTile ? currTile.id:"undefined");
+    //console.log("otherTile:",otherTile ? otherTile.id:"undefined");
+    //if(!currTile||!otherTile){
+        //console.log("Error:currTile or ortherTile is undefined");
+       // return;
+    //}
+    //console.log("beforee swap-currTile:",currTile.id,"otherTile:",otherTile.id);
+    //console.log("beforee swap-currTile.src:",currTile.src,"otherTile:",otherTile.src);
+    if(!currTile){
+       currTile=window.currTile;
+        return;
+    }
+        if(!currTile||!otherTile){
+            console.log("Error:currTile or ortherTile is undefined");
+        return;
+        }
+    console.log("beforeSwap-currTile",currTile.id,"otherTile:",otherTile.id);
+    /*let currImg= currTile.src;
+    let otherImg=otherTile.src;
+    currTile.src=otherImg;
+    otherTile.src=currImg;*/
+    let currImg=currTile.src;
+    let otherImg=otherTile.src;
+    currTile.src=otherImg;
+    otherTile.src=currImg;
+    console.log("swap Scessful",currTile.id,"<->",otherTile.id);
 
 }
